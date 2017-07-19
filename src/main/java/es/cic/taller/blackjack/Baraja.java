9f56e98a22e46315d2b3fa2d66aecf5c1232de3c
Clare <claretgb@gmail.com>
2017-07-19 10:58:05 +0200

    Creo la clase Baraja (necesita la clase Tapete para funcionar).

---
+++ src/main/java/es/cic/taller/blackjack/Baraja.java
@@ -0,0 +1,67 @@
+package es.cic.taller.blackjack;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Baraja {
+	private List<Carta> listaCartas = new ArrayList<>();
+	
+	private List<Carta> listaMonton = new ArrayList<>();
+	
+	public Baraja() {
+		generarCartas();
+	}
+	private void generarCartas() {
+		listaCartas.addAll(getPalo(Palo.CORAZONES));
+		listaCartas.addAll(getPalo(Palo.DIAMANTES));
+		listaCartas.addAll(getPalo(Palo.PICAS));
+		listaCartas.addAll(getPalo(Palo.TREBOLES));
+		barajear();
+	}
+	
+	private List<Carta> getPalo(Palo palo) {
+		
+		List<Carta> listaCartasPalo = new ArrayList<>();
+		
+		for (int i= 1; i <= 10; i++) {
+			Carta carta = new Carta();
+			Numero numero = Numero.getNumero(i);
+			carta.setNumero(numero);
+			carta.setPalo(palo);
+			listaCartasPalo.add(carta);
+		}
+		return listaCartasPalo;
+	}
+	
+	public void barajear() {
+		Collections.shuffle(listaCartas);
+	}
+	
+	public void resetear() {
+		listaCartas.clear();
+		listaMonton.clear();
+		
+		generarCartas();
+	}
+	
+	private Carta getCarta() {
+		if (listaCartas.isEmpty()) {
+			listaCartas.addAll(listaMonton);
+			barajear();
+			listaMonton.clear();
+		}
+		Carta carta = listaCartas.remove(0);
+		return carta;		
+	}
+	
+	public Tapete getTapete() {
+		Tapete tapete = new Tapete();
+		tapete.setCarta1(getCarta());
+		tapete.setCarta2(getCarta());
+		tapete.setCarta3(getCarta());
+		tapete.setCarta4(getCarta());
+	
+		return tapete;
+	}
+}
